47f030dc1e7170430755745687c133933b18cbb5
shommy14 <36086060+shommy14@users.noreply.github.com>
2020-01-28 19:08:26 +0100

    First commit

---
+++ app/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'userId' );
+    }
+    public function category()
+    {
+        return $this->belongsTo('App\Category', 'catId');
+    }
+    public function bid()
+    {
+        return $this->hasOne('App\Bid', 'product_id');
+    }
+}
---
+++ database/migrations/2020_01_28_161318_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('payment');
+            $table->string('shipment');
+            $table->boolean('sold')->default(0);
+            $table->unsignedInteger('catId')->nullable(false);
+            $table->unsignedInteger('userId')->nullable(false);
+            $table->foreign('catId')->references('id')->on('categories');
+            $table->foreign('userId')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
